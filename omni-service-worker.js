3128883de78e7d89e796bad601e0062c8f2d10c1
Benson Liao <skylock777@gmail.com>
2025-04-08 12:01:50 +0800

    Update omni-service-worker.js

--- omni-service-worker.js
+++ omni-service-worker.js
@@ -1,2 +1,3 @@
 // test worker
-importScripts('https://bensonliao.github.io/service-worker-sandbox-site/omni-service-worker.js');
+const v = new Date().getTime();
+importScripts(`https://bensonliao.github.io/service-worker-sandbox-site/omni-service-worker.js?v=${v}`);
